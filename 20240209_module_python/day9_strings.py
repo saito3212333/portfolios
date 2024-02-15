10bb22d5e90795a6c79cc6ed14ad7d4d21c54c70
ITO <holpitasu3618@gmail.com>
2024-02-14 22:05:25 -0500

    new save

--- 20240209_module_python/day9_strings.py
+++ 20240209_module_python/day9_strings.py
@@ -89,6 +89,7 @@ class StringEngine:
         and re-set the effective date so that an orderly implementation of the law may be effected"."""
 
         print(text1)
+        # 0--7 の８個を抽出
         sbstr1 = text1[:8]
         print(sbstr1)
 
@@ -100,6 +101,8 @@ class StringEngine:
 
         # start:stop:step
         print(sbstr2[::-1])
+        # リバースで逆むけて、リストでリストに、最後にジョイン
+        print(list(reversed(sbstr2)))
         print(''.join(list(reversed(sbstr2))))
         print(sbstr2[::2])
         print()
@@ -126,12 +129,17 @@ class StringEngine:
         price = 42000
         discount = -2000
 
+        # ${2:,.2f}: price変数の値が挿入されます。この値は、3桁ごとにカンマが挿入され、小数点以下2桁まで表示されます。
+        # {3:,.2f}: discount変数の値が挿入されます。同様に、3桁ごとにカンマが挿入され、小数点以下2桁まで表示されます。
         car_str = 'We can offer you a {0} {1} for ${2:,.2f}  with a discount of {3:,.2f}.'
         print(car_str.format(car, model,price, discount ))
         print('binary format: {:b}'.format(discount))
         print('decimal format: {:d}'.format(discount))
         print('octal format: {:o}'.format(discount))
         print('hex format: {:x}'.format(discount))
+        # それぞれ、データをバイナリ、10進、8進、16進形式に変換して表示します。
+        # {: b}はバイナリ、{: d}は10進、{: o}は8進、{: x}は16進形式を表します。
+        #
 
         print()
         print(f'We can offer you a {car} {model} for ${price:,.2f}  with a discount of {discount:,.2f}.')
@@ -140,6 +148,7 @@ class StringEngine:
         print()
         print(f'The sqr. root of 2 is {round(numpy.sqrt(2), 3)},')
 
+        # \をつければ、特殊文字を表示できますねー
         print()
         print("I have \"two\" dogs.")
         print("folder1\\filename")
@@ -149,27 +158,40 @@ class StringEngine:
         print(str1)
         print(str1.upper()) # ALl CAPS
         print(str1.lower()) # all simple
-        print(str1.swapcase()) # Case switched
+        print(str1.swapcase()) # Case switched　大文字を小文字に小文字を大文字に。
         print(str1.capitalize()) # First letter capitalized
-        print(str1.title()) # The first letter of each word capitalized
+        print(str1.title()) # The first letter of each word capitalized　文字それぞれの先頭を大文字に
 
         print()
         str2 = f'    The sqr. root of 2 is {round(numpy.sqrt(2), 3)}.    '
         print(str1)
         print(str2)
-        print(str2.strip())
-        print(str2.lstrip())
-        print(str2.rstrip())
+        print(str2.strip())#str2の前後の空白を削除して表示します。
+        print(str2.lstrip())#str2の左側の空白を削除して表示します。
+        print(str2.rstrip())#str2の右側の空白を削除して表示します。
 
         print()
         str2 = 'I like apples. Sometimes apples are red, and sometimes apples are green.'
+
+        # find()メソッドは、指定された部分文字列（この場合は'apples'）が
+        # 最初に見つかったインデックスを返します。もし見つからない場合は、-1を返します。
+        # この行では、最初に'apples'が見つかったインデックスを表示します。
         print(str2.find('apples'))
+
+        # index()メソッドは、指定された部分文字列が最初に見つかったインデックスを返しますが、
+        # 見つからない場合はValueErrorを発生させます。第二引数と第三引数は探索範囲を指定します。
+        # この行では、最初の15文字内で'apples'が見つかったインデックスを表示します。
         print(str2.index('apples', 0, 15))
 
+        # rfind()メソッドは、指定された部分文字列が最後に見つかったインデックスを返します。
+        # この行では、最後に'apples'が見つかったインデックスを表示します。
         print(str2.rfind('apples'))
 
         print()
+
+        # アルファベットだけで構成されていますか？
         print(str2.isalpha())
+        print('abc'.isalpha())
 
         str3 = '76.4'
         str4 = '9'
@@ -179,25 +201,28 @@ class StringEngine:
         print(str4.isalpha())
         print(str5.isalpha())
 
+        # これは数字のみ.もだめです。
         print()
         print(str3.isdigit())
         print(str4.isdigit())
         print(str5.isdigit())
-
+        # アルファベットもしくは数字
         print()
         print(str3.isalnum())
         print(str4.isalnum())
         print(str5.isalnum())
 
+        # １０進数のみです。
         print()
         print(str3.isdecimal())
         print(str4.isdecimal())
         print(str5.isdecimal())
-
+        #isascii()メソッドは、文字列がASCII文字のみで構成されているかどうかを示すブール値を返します。
+        # str3、str4、str5のすべての文字はASCII文字であるため、すべての場合にTrueが返されます。
         print()
-        print(str3.isascii())
-        print(str4.isascii())
-        print(str5.isascii())
+        # print(str3.isascii())
+        # print(str4.isascii())
+        # print(str5.isascii())
 
         print()
         str6 = '''We cordially invite our current and former students, 
@@ -209,9 +234,15 @@ class StringEngine:
         print()
 
         # EXTRACT THE WORDS/TOKENS
+        # split()メソッドを使用して、文字列を空白文字で分割します。
+        # 結果はリストとして返されます。
         print(str6.split())
 
         # EXTRACT THE UNIQUE WORDS/TOKENS
+        # split()メソッドで文字列を空白文字で分割し、
+        # その後重複を除去するためにset()関数を使用します。
+        # セットで重複削除できるんですね。知らんかった。
+
         lst_words = set(str6.split())
         print(lst_words)
 
@@ -230,12 +261,23 @@ class StringEngine:
         # EXTRACT THE COUNT/FREQUENCY OF UNIQUE WORDS/TOKENS
         print()
         bow_str7 = str7.lower().split()
+        # 肝はcounter関数
         word_count = Counter(bow_str7)
+
+        print('_______________________')
+        # <class 'collections.Counter'>　これがデータタイプです。
+        print(type(word_count))
         print(word_count)
         print()
+        # 　辞書にしてあげるといい感じになります。
         word_count_dict = dict(word_count)
         print()
-
+        # sorted()関数は、指定されたイテラブル
+        # （ここではword_count_dict.items()で
+        # 辞書のキーと値のペアがイテレートされます）をソートします。
+        # reverse=True引数により、降順（大きい値から小さい値へ）でソートされます。
+        # key引数は、ソートの際に使用される関数またはキー関数を指定します。
+        # ここでは、lambda関数を使用して各アイテムの値（item[1]）をキーとして指定しています。
         word_count_dict_sorted = dict(sorted(word_count_dict.items(), reverse= True,
                                         key = lambda item : item[1]))
 
@@ -252,20 +294,32 @@ class StringEngine:
         lst2 = [5,6,10]
 
         z_list = zip(lst1, lst2)
-        print(z_list)
+        print('this is ',z_list)
         print(list(z_list))
         z_list = zip(lst1, lst2)
         print(dict(z_list))
-
+        # イテレーター（iterator）は、要素を1つずつ順番に提供するオブジェクトです。
+        # イテレーターは主にループや反復処理に使用されます。
+        # Pythonでは、多くの場面でイテレーターが使用されますが、特にforループやzip()、
+        # enumerate()、map()、filter()などの組み込み関数でよく見られます。
+        #
+        # イテレーターは、iter()関数で生成されるオブジェクトで、
+        # next()関数を使用して次の要素に進むことができます。
+        # また、forループを使うことで、イテレーターから要素を順番に取り出すこともできます。
         print()
         # CONSTANTS
-
+        # 特殊文字の扱い方。
         print('ascii_uppercase: ', string.ascii_uppercase)
         print('ascii_lowercase: ', string.ascii_lowercase)
         print('ascii_letters: ', string.ascii_letters)
         print('punctuation: ', string.punctuation)
         print('digits: ', string.digits)
 
+        # ascii_uppercase:  ABCDEFGHIJKLMNOPQRSTUVWXYZ
+        # ascii_lowercase:  abcdefghijklmnopqrstuvwxyz
+        # ascii_letters:  abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+        # punctuation:  !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+        # digits:  0123456789
     def doTokenTask(self):
         text1 = '''Before we get into more details about the different types of paragraph, let’s explain what a paragraph really is. No, five consecutive sentences don’t make up a paragraph. And no, a paragraph doesn’t have to be half a page long. So, what is a paragraph? Regardless of all the paragraph organization types, the definition of a paragraph is pretty simple. A paragraph is a “group of sentences or a single sentence that forms a unit” (Lunsford and Connors, 1995, p.116).'''
         # Split the text into a list of words and call it xWords.
@@ -273,18 +327,20 @@ class StringEngine:
         # Remove any special character in all the words in xWords by replacing it with
         # a space character. For instance, if a word is 'person"'
         # the result should replace it with person.
-        xWords = text1.lower().split()
-        xWords = [i.strip() for i in xWords]
-
-        text2 = ' '.join(xWords)
-        text2_lst = list(text2)
-
+        print(text1)
+        xWords = text1.lower().split()# 全部小文字にしてスプリット
+        xWords = [i.strip() for i in xWords] # 空白を削除　スプリットされた一文字ずつ
+        print(xWords)
+        text2 = ' '.join(xWords)#また結合
+        text2_lst = list(text2)#リスト化したら１文字ずつ格納
+        print(text2_lst)
         counter = 0
         for c in text2_lst:
+            # のっと　＋　（）が必要です。
             if not(c.isalnum()):
-                print(counter)
+                #print(counter)
                 text2_lst[counter] = ' '
-
+                # c = ' '
             counter += 1
 
         text3 = ''.join(text2_lst)
@@ -298,8 +354,8 @@ class StringEngine:
 
 
 
-ee2 = ExEngine()
-ee2.do_df_ex()
-#se1 = StringEngine()
+#ee2 = ExEngine()
+#ee2.do_df_ex()
+se1 = StringEngine()
 #se1.recapStringBasics()
-#se1.doTokenTask()
+se1.doTokenTask()
